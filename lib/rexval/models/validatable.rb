00ddb86e3c95d7971187113ed93b2f76d4df0d83
colin chun <caleon@gmail.com>
2012-01-29 09:16:41 -0800

    - fixing bug resulting in regexp validating fields more than once,  - version bump

--- lib/rexval/models/validatable.rb
+++ lib/rexval/models/validatable.rb
@@ -12,22 +12,24 @@ module Rexval
         
         class_attribute :rexval_fields, :instance_writer => false
         self.rexval_fields ||= []
+        
+        # validate *rexval_fields, :regexp => true
       end
       
       module ClassMethods
         Rexval::Models.config(self, :default_regexp)
         
-        def validates(*attributes)
-          defaults = attributes.extract_options!
-          
-          if !(special_fields = self.rexval_fields & attributes).empty?
-            super(*special_fields.push(defaults.merge(:regexp => true)))
-          end
-          
-          if !(normal_fields = attributes - self.rexval_fields).empty?
-            super(*normal_fields.push(defaults))
-          end
-        end
+        # def validates(*attributes)
+        #   defaults = attributes.extract_options!
+        #   
+        #   if !(special_fields = self.rexval_fields & attributes).empty?
+        #     super(*special_fields.push(defaults.merge(:regexp => true)))
+        #   end
+        #   
+        #   if !(normal_fields = attributes - self.rexval_fields).empty?
+        #     super(*normal_fields.push(defaults))
+        #   end
+        # end
                 
       end
     end
